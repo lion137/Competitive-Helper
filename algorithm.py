7eab90acff57ba4ec1bd18a405306580566e62b0
lion137 <lion137@gmail.com>
2018-12-27 22:10:52 +0000

    remote

---
+++ algorithm.py
@@ -0,0 +1,333 @@
+# Module with various utilities to competitive mathematics/programming.
+# copyleft 2018 lion137
+# Sources: P. Norvig: Pytudes, Stackoverflow, Wikipedia
+# http://en.literateprograms.org/Miller-Rabin_primality_test_(Python).
+
+
+from operator import mul
+import random
+from functools import reduce
+from fractions import Fraction
+from collections import Counter
+from itertools import combinations_with_replacement, repeat
+from math import log, pow, gcd
+
+
+class Matrix:
+	
+	a = None
+	
+	def __init__(self, n, xs=None):
+		arr=[[0 for x in range(n)] for x in range(n)]
+		self.a = arr
+		if xs:
+			for i in range(len(xs)):
+				self.a[i] = xs[i]
+	
+	def zero(self):
+		return Matrix(len(self)) 
+			
+	def id(self):
+		length = len(self)
+		arr = [[0 for x in range(length)] for x in range(length)]
+		for i in range(length):
+			for j in range(length):
+				if ( i == j):
+					arr[i][j] = 1
+		return Matrix(length, arr)
+	
+	def __repr__(self):
+		return str(self.a)
+	
+	def __eq__(self, other):
+		return self.a == other.a
+	
+	def set_at(self, e, i, j):
+		self.a[i][j] = e
+	
+	def get_at(self, i, j):
+		return self.a[i][j]
+	
+	def __mul__(self, other):
+		
+		length = len(self.a[0])
+		ll = [0] * length
+		lll = []
+		for _ in range(length):
+			lll.append(ll)
+		C = Matrix(length)
+		
+		for i in range(length):
+			for j in range(length):
+				s = 0
+				for k in range(length):
+					s = s + self.a[i][k] * other.a[k][j]
+				C.a[i][j] = s
+		return C
+	
+	def mod_mul_mat(self, other, m):
+		
+		length = len(self.a[0])
+		ll = [0] * length
+		lll = []
+		for _ in range(length):
+			lll.append(ll)
+		C = Matrix(length)
+		
+		for i in range(length):
+			for j in range(length):
+				s = 0
+				for k in range(length):
+					s = (s +  ( ((self.a[i][k] % m) *  (other.a[k][j] % m)) % m ) ) % m
+				C.a[i][j] = s
+		return C
+	
+	def __len__(self):
+		return len(self.a)
+	
+	def __mod__(self, m):
+		length = len(self)
+		for i in range(length):
+			for j in range(length):
+				self.a[i][j] = self.a[i][j] % m
+		return self
+
+
+# General purpose internal functions
+
+def zero(a):
+	if isinstance(a, int) or isinstance(a, float): return 0
+	if isinstance(a, complex): return 0 +0j
+	if isinstance(a, Matrix): return a.zero()
+
+def identity(a):
+	if isinstance(a, int) or isinstance(a, float): return 1
+	if isinstance(a, complex): return 1 +0j
+	if isinstance(a, Matrix): return a.id()
+
+def mod(e, m):
+	if isinstance(e, int): return e % m
+	if isinstance(e, Matrix): return e % m
+
+def odd(n):
+	return n % 2 == 1
+	
+def half(a):
+	if isinstance(a, complex): return a / 2
+	if isinstance(a, int): return a >> 1
+
+def mod_mul(a, b, m):
+	if isinstance(a, int): return ( (a % m) * (b % m)) % m
+	if isinstance(a, Matrix): return a.mod_mul_mat(b, m)
+
+def mat_vec(a, xs):
+	"""Square matrix times vector multiplication"""
+	limit = len(xs)
+	result = [0] * limit
+	for i in range(limit):
+		s = zero(xs[0])
+		for k in range(limit):
+			s = s + a.a[i][k] * xs[k]
+		result[i] = s
+	return result 
+
+def mat_vec_mod(a, xs, m):
+	limit = len(xs)
+	result = [0] * limit
+	for i in range(limit):
+		s = zero(xs[0])
+		for k in range(limit):
+			s = (s + mod_mul(a.a[i][k],  xs[k], m) ) % m
+		result[i] = s
+	return result
+	
+
+#  Prime Factoroization, Number Theoretic, internal  functions
+
+def sqrt_int(n):
+    "Integer square root (rounds down)."
+    return int(n ** 0.5)
+
+def mult(nums):
+	result = 1
+	for n in nums:
+		result *= n
+	return result
+
+# Functions
+
+def power(x, n, f=mul):
+	"""Takes regular type, x (square matrix, number or complex)
+	and without specified f, returns its power to n"""
+	if n == 0: return identity(x)
+	y = identity(x)
+	while n > 1:
+		if not odd(n):
+			x = f(x, x)
+			n = half(n)
+		else:
+			y = f(x, y)
+			x = f(x, x)
+			n = (n - 1) // 2
+	return f(x, y)
+	
+def pow_mod(a, n, m, f=mod_mul):
+	"""Takes a regular type element a (matrices and numbers), n, m 
+	   and f (operator) returns a to power n modulo m.
+	   Default f is multiplication modulo of numbers and matrices"""
+	if 1 == m: return zero(a)
+	if a == zero(a): return zero(a)
+	result = identity(a)
+	a = mod(a, m)
+	while n > 0:
+		if odd(n):
+			result = f(a, result, m)
+		n = half(n)
+		a = f(a, a, m)
+	return result
+
+def recurrence(n, a_vec, c_vec):
+	"""Computes nth value of recurrence, 
+	for info about a_vec and c_vec see: 
+	https://github.com/lion137/Linear_Recurrence_Solver"""
+	limit = len(a_vec)
+	A = Matrix(limit)
+	for i in range(limit):
+		A.a[0][i] = a_vec[i]
+	for i in range(1, limit):
+		for j in range(limit):
+			if i - j == 1:
+				A.a[i][j] = 1
+	if n < len(c_vec): return c_vec[n]
+	c_vec = c_vec[::-1]
+	return mat_vec(power(A, n + 1 - limit), c_vec)[0]
+	
+	
+def recurrence_mod(n, a_vec, c_vec, m):
+	"""Computes the nth value of the given recurrence
+	modulo m, see: https://github.com/lion137/Linear_Recurrence_Solver"""
+	limit = len(a_vec)
+	A = Matrix(limit)
+	for i in range(limit):
+		A.a[0][i] = a_vec[i]
+	for i in range(1, limit):
+		for j in range(limit):
+			if i - j == 1:
+				A.a[i][j] = 1
+	if n < len(c_vec): return c_vec[n]
+	c_vec = c_vec[::-1]
+	return mat_vec_mod(pow_mod(A, n + 1 - limit, m), c_vec, m)[0]
+
+# Number Theoretic, Primes
+
+def is_prime(n):
+    """
+    Test if prime, Miller Rabin
+	"""
+    if n == 1: return False
+    d = n - 1
+    s = 0
+    while d % 2 == 0:
+        d >>= 1
+        s += 1
+    for repeat in range(20):
+        a = 0
+        while a == 0:
+            a = random.randrange(n)
+        if not miller_rabin_pass(a, s, d, n):
+            return False
+    return True
+
+
+def miller_rabin_pass(a, s, d, n): # is_prime helper
+    a_to_power = pow_mod(a, d, n)
+    if a_to_power == 1:
+        return True
+    for i in range(s - 1):
+        if a_to_power == n - 1:
+            return True
+        a_to_power = (a_to_power * a_to_power) % n
+    return a_to_power == n - 1
+
+
+def sieve(n):
+	"""Create an iterable generator of primes, with initial cache of all primes <= n."""
+	N = n // 2 
+	_sieve = [True] * N
+	for i in range(3, sqrt_int(n) + 1, 2):
+		if _sieve[i // 2]: # i is prime
+			# Mark start, start + i, start + 2i, ... as non-prime
+			start = i ** 2 // 2
+			_sieve[start::i] = repeat(False, len(range(start, N, i)))
+	_list = [2] + [2*i+1 for i in range(1, N) if _sieve[i]]
+	_set  = set(_list)
+	maxn  = n # We have tested for all primes < self.maxn
+	return [_list, _set, n]
+
+def primes(n):
+	"""Primes up tu n"""
+	return sieve(n)[0]
+
+milion = 1000000
+prime_nums = sieve(10 * milion)
+
+def factorize(n):
+	"""Prime factorization of 1 < n < 10^12"""
+	result = []
+	if n >= 2 * milion:
+		for p in prime_nums[0]:
+			while n % p == 0:
+				result.append(p)
+				n //= p
+			if n < 2 * milion:
+				break
+			if is_prime(n):
+				result.append(n)
+				return result
+	for p in prime_nums[0]:
+		while n % p == 0:
+			result.append(p)
+			n //= p
+	return result
+
+def distinct_factors(n):
+	return set(factorize(n))
+
+def totient(n):
+	"""Eulers Totient function: https://en.wikipedia.org/wiki/Euler's_totient_function"""
+	return int( n * mult(1 - Fraction(1, p) for p in set(factorize(n))))
+
+def ndivisors(n):
+	"""Returns number of divisors"""
+	exps = Counter(factorize(n)).values()
+	return mult(a + 1 for a in exps)
+
+def divisors(n):
+	"""Returns the all divisors of n"""
+	if n == 1: return 1
+	factors = list(distinct_factors(n))
+	length = int(log(n, min(factors))) + 1
+	c_tail = list(combinations_with_replacement(list(range(length, -1, -1)), len(factors)))
+	comb = list(combinations_with_replacement(list(range(length + 1)), len(factors))) + c_tail
+	#print(comb)
+	result = []
+	for e in comb:
+		tmp = []
+		for p, c in zip(factors, e):
+			tmp.append(int(pow(p, c)))
+		m = mult(tmp)
+		if m <= n // 2 and n % m == 0:
+			result.append(m)
+	result = list(set(result))
+	result.append(n)
+	result.sort()
+	return result
+
+def sdivisors(n):
+	"""Returns sum of proper divisors, less < n"""
+	return sum(divisors(n)) - n
+
+
+def lcm(a, b):
+	"""Least Common Multiple of a, b"""
+	return abs(a * b) // gcd(a, b)
---
+++ tests.py
@@ -0,0 +1,224 @@
+# tests
+
+
+import sys
+sys.path.append(".")
+
+from algorithm import *
+#from classes import *
+#from functions import *
+#from itertools import combinations_with_replacement
+
+
+def tests():
+	
+	print("Test Zero int")
+	assert zero(123) == 0
+	
+	print("Test Zero float")
+	assert zero(123.1) == 0
+	
+	print("Test Zero complex")
+	assert zero(1 + 2j) == 0 + 0j
+	
+	print("Test Zero matrix")
+	m1 = Matrix(2, [[1, 1], [2, 3]])
+	zer = Matrix(2);
+	assert m1.zero() == zer
+	
+	print("Test Matrix identity id")
+	m1 = Matrix(2, [[1, 1], [2, 3]])
+	m2 = Matrix(2, [[1, 0], [0, 1]])
+	assert m1.id() == m2
+	
+	print("Test Matrix Multiplication")
+	m0 = Matrix(2, [[1, 1], [1, 1]])
+	m1 = Matrix(2, [[1, 1], [1, 1]])
+	m2 = Matrix(2, [[2, 2], [2, 2]])
+	assert m0 * m1 == m2
+	assert m0.id() * m0.id() == m2.id()
+	
+	print("Test Matrix Mod")
+	m0 = Matrix(2, [ [2, 7], [11, 23] ])
+	m1 = Matrix(2, [ [2, 2], [1, 3] ])
+	assert m0 % 5 == m1
+	
+	print("Test Identity int")
+	assert identity(23) == 1
+	
+	print("Test Identity float")
+	assert identity(23.01) == 1
+	
+	print("Test Identity complex")
+	assert identity(23 + 23j) == complex(1)
+	
+	print("Test Identity Matrix")
+	m0 = Matrix(2, [ [2, 7], [11, 23] ])
+	assert identity(m0) == m0.id()
+	
+	print("Test Modulo in int")
+	assert mod(23, 5) == 3
+	
+	print("Test Modulo Matgrix")
+	m0 = Matrix(2, [ [2, 7], [11, 23] ])
+	m1 = Matrix(2, [ [2, 2], [1, 3] ])
+	assert mod(m0, 5) == m1
+	
+	print("Test Odd")
+	assert odd(3) == True
+	assert odd(2) == False
+	
+	print("Test half Complex")
+	assert half(2 + 2j) == 1 + 1j
+	
+	print("Test Modulo Mul int")
+	assert mod_mul(5, 7, 3) == 2
+	
+	print("Test Modulo Mul Matrix")
+	m0 = Matrix(2, [ [2, 4], [7, 3] ])
+	m1 = Matrix(2, [ [1, 2], [3, 4] ])
+	m2 = Matrix(2, [ [4, 0], [1, 1] ])
+	assert mod_mul(m0, m1, 5) == m2
+	
+	
+	print("Test Power Modulo in Inegers")
+	assert pow_mod(2, 10, 1000) == 24
+	assert pow_mod(3, 3, 5) == 2
+	
+	print("Test Power Modulo Matrix")
+	m0 = Matrix(2, [ [2, 4], [7, 3] ])
+	m1 = Matrix(2, [ [2, 0], [0, 2] ])
+	m = Matrix(2, [ [1, 1], [1, 0] ])
+	assert pow_mod(m0, 2, 5) == m1
+	assert pow_mod(m, 1234567891011, 1000000007).a[0][1] == 316399615 # Fibonacci
+	
+	print ("Test Power in integers")
+	assert power(2, 3) == 8
+	assert power(2, 0) == 1
+	assert power(2, 1) == 2
+	assert power(3, 5) == 243
+	
+	
+	print ("Test Power in Matrices")
+	m0 = Matrix(2, [ [2, 4], [7, 3] ])
+	m1 = Matrix(2, [ [32, 20], [35, 37] ])
+	m_id = Matrix(2, [[1, 0], [0, 1]])
+	assert power(m0, 2) == m1
+	assert power(m0, 0) == identity(m0)
+	assert power(m0, 1) == m0
+	assert power(m_id, 1000000) == m_id
+	
+	print ("Matrix times Vector")
+	m_id = Matrix(2, [[1, 0], [0, 1]])
+	m0 = Matrix(2, [[1, 2], [3, 4]])
+	assert mat_vec(m_id, [1, 1]) == [1, 1]
+	assert mat_vec(m0, [1, 2]) == [5, 11]
+	
+	print ("Test Recurrence Solver in Integers")
+	assert recurrence(0, [1, 1], [0, 1]) == 0
+	assert recurrence(1, [1, 1], [0, 1]) == 1
+	assert recurrence(7, [1, 1], [0, 1]) == 13
+	
+	# Recurrence: f(0) = 0, f(1) = 1, f(2) = 2
+	# f(n) = 2f(n - 1) + 3f(n - 3) 
+	def f(n):
+		if n == 0: return 0
+		elif n == 1: return 1
+		elif n == 2: return 2
+		else:
+			return 2 * f(n - 1) + 3 * f(n - 3)
+			 
+	assert recurrence (13, [2, 0, 3], [0, 1, 2]) == f(13)
+	assert recurrence (0, [2, 0, 3], [0, 1, 2]) == f(0)
+	assert recurrence (1, [2, 0, 3], [0, 1, 2]) == f(1)
+	assert recurrence (2, [2, 0, 3], [0, 1, 2]) == f(2)
+	
+	print ("Test Matrix Vector Multiplication Modulo")
+	m_id = Matrix(2, [[1, 0], [0, 1]])
+	m0 = Matrix(2, [[1, 2], [3, 4]])
+	assert mat_vec_mod(m0, [1, 2], 1) == [0, 0]
+	assert mat_vec_mod(m0, [1, 2], 2) == [1, 1]
+	assert mat_vec_mod(m0, [1, 2], 7) == [5, 4]
+	assert mat_vec_mod(m0, [1, 2], 20) == [5, 11]
+	
+	print ("Test Recurrence Modulo")
+	def fibo(n):
+		a, b = 0, 1
+		while n > 0:
+			a, b = b, a + b
+			n -= 1
+		return a
+	par = 7
+	assert fibo(par) == 13
+	assert recurrence_mod(13, [1, 1], [0, 1], 13) == fibo(13) % 13
+	assert recurrence_mod(150, [1, 1], [0, 1], 100007) == fibo(150) % 100007
+	
+	print ("")
+	print ("Primes, Number Theory Tests")
+	
+	print ("Prime Sieve Tests")
+	primes = sieve(10)
+	assert primes[0] == [2, 3, 5, 7]
+	assert primes[1] == {2, 3, 5, 7}
+	assert primes[2] == 10
+	
+	print ("Test is_prime")
+	assert is_prime(1) == False
+	assert is_prime(42) == False
+	assert is_prime(2) == True
+	assert is_prime(3) == True
+	assert is_prime(1000000) == False
+	assert is_prime(162259276829213363391578010288127) == True
+	
+	print ("Test Factorization")
+	assert factorize(3000000) == [2, 2, 2, 2, 2, 2, 3, 5, 5, 5, 5, 5, 5]
+	assert factorize(1) == []
+	assert factorize(2) == [2]
+	assert factorize(35) == [5, 7]
+	assert factorize(12345678910) == [2, 5, 1234567891]
+	
+	print("Test Totient Function")
+	assert totient(100) == 40
+	assert totient(1) == 1
+	assert totient(137) == 136
+	assert totient(13) == 12
+	assert totient(1234567890) == 329040288
+
+	print ("Test Number Of Divisors")
+	assert ndivisors(12) == 6
+	assert ndivisors(7) == 2
+	#print(list(combinations_with_replacement(list(range(6)), 3)))
+	
+	print("Test Divisors")
+	assert divisors(1) == 1
+	assert divisors(2) == [1, 2]
+	assert divisors(12) == [1, 2, 3, 4, 6, 12]
+	assert divisors(137) == [1, 137]
+	assert divisors(2048) == [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048]
+	assert len(divisors(2048)) == 12
+	assert len(divisors(100)) == 9
+	assert divisors(100) == [1, 2, 4, 5, 10, 20, 25, 50, 100]
+	
+	print ("Test Sum of Proper Divisors")
+	assert sdivisors(12) == 16
+	assert sdivisors(137) == 1
+	assert sdivisors(100) == 117
+	
+	print ("Test of Least Common Multiple lcm")
+	assert lcm(21, 6) == 42
+	assert lcm(3, 3) == 3
+	
+	
+	
+	
+tests()
+
+
+
+
+
+
+
+
+
+
